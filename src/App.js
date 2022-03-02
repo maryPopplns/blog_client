448c3c4f8bb4762ba0744faf84c6babd4a97df54
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-02 16:37:45 -0700

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  return <div className='App'></div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div className='App'>
+      <button onClick={() => setCount((prev) => prev + 1)}>press</button>
+      <div>{count}</div>
+    </div>
+  );
 }
 
 export default App;

365be2599e474f83f30aadb21f159f329428e3d3
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-03 13:48:44 -0700

    basic routes complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.scss';
-import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
+  const homeLink = '</Blog>';
   return (
     <div className='App'>
-      <button onClick={() => setCount((prev) => prev + 1)}>press</button>
-      <div>{count}</div>
-      <Link to=''>index</Link>
-      <Link to='login'>login</Link>
-      <Link to='signup'>sign up</Link>
-      <Link to='98502805498'>blogpost</Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to=''>{homeLink}</Link>
+          </li>
+          <li>
+            <Link to='login'>Login</Link>
+          </li>
+          <li>
+            <Link to='signup'>Sign Up</Link>
+          </li>
+        </ul>
+      </nav>
       <Outlet />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './index.scss';
+import './reset.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';

130d09d9231211f6f7f21be696bbc68960b077bc
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-03 10:34:45 -0700

    removed test component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
+import './index.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.scss';
 import App from './App';
-import NewOne from './newOne';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/blog_client' element={<App />} />
-        <Route path='/blog_client/new' element={<NewOne />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,

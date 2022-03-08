f7aed9f7f49a9dcce0b01487e5407163ae462dd5
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-08 14:25:02 -0700

    removed redux, no need for it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
@@ -12,18 +10,16 @@ import Login from './components/login/login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/blog_client' element={<App />}>
-            <Route index element={<AllBlogs />} />
-            <Route path='login' element={<Login />} />
-            <Route path='signup' element={<SignUp />} />
-            <Route path=':blogPost' element={<BlogPost />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/blog_client' element={<App />}>
+          <Route index element={<AllBlogs />} />
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<SignUp />} />
+          <Route path=':blogPost' element={<BlogPost />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );

00ae622b5c9f064473770cf813e2a35f3bc12056
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-08 14:35:57 -0700

    fetching homepage data successfully

--- src/components/allBlogs/allBlogs.js
+++ src/components/allBlogs/allBlogs.js
@@ -1,6 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 export default function AllBlogs() {
-  return <div className='blogpost'>index</div>;
+  const [user, setUser] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      // TODO calls to API to get all blogs
+      const response = await fetch('https://knight-blog.herokuapp.com/');
+      const { user, blogs } = await response.json();
+      await setUser(user);
+      await setBlogs(blogs);
+    })();
+  }, []);
+
+  return <div className='blogpost'>{console.log(blogs)}</div>;
 }

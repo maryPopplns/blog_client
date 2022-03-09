1c6114edfbb5f7d60372bfbc3cd39dd794a27219
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-09 13:13:17 -0700

    base login form complete

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,6 +1,25 @@
 import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import './login.scss';
 
 export default function Login() {
-  return <div className='login'>login</div>;
+  return (
+    <div className='login'>
+      <form action='https://knight-blog.herokuapp.com/login/local'>
+        <div classname='username-field'>
+          <label for='username'>Username</label>
+          <input type='text' id='username' name='username'></input>
+        </div>
+        <div className='password-field'>
+          <label for='password'>Password</label>
+          <input type='text' id='password' name='password'></input>
+        </div>
+        <button type='submit'>submit</button>
+      </form>
+      <a href='https://knight-blog.herokuapp.com/login/google'>google</a>
+    </div>
+  );
 }
+
+// create a form to login with username/password
+//  create a button to login to google

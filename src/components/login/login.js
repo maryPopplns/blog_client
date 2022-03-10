183f88963aea2d588f6f287e05263f1bb0487390
Spencer Knight <spencer.knightusn@gmail.com>
2022-03-10 09:34:51 -0700

    login via api pulling successfully

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,30 +1,60 @@
+import axios from 'axios';
 import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import './login.scss';
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function submitFormHandler(event) {
+    event.preventDefault();
+    const loginData = { username, password };
+    let formBody = [];
+    for (const property in loginData) {
+      const encodedKey = encodeURIComponent(property);
+      const encodedValue = encodeURIComponent(loginData[property]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    formBody = formBody.join('&');
+
+    fetch('https://knight-blog.herokuapp.com/login/local', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formBody,
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div className='login'>
-      <form
-        method='POST'
-        action='https://knight-blog.herokuapp.com/login/local'
-      >
+      <form id='loginForm' onSubmit={submitFormHandler}>
         <div className='username-field'>
           <label htmlFor='username'>Username</label>
-          <input type='text' id='username' name='username'></input>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          ></input>
         </div>
         <div className='password-field'>
           <label htmlFor='password'>Password</label>
-          <input type='password' id='password' name='password'></input>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          ></input>
         </div>
         <input type='submit'></input>
       </form>
-      <a
-        className='google-login'
-        href='https://knight-blog.herokuapp.com/login/google'
-      >
-        Google Login
-      </a>
     </div>
   );
 }
